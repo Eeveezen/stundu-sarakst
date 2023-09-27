6edb3461c5d397dab6c924d9d97b5643daacd045
Eeveezen <81543263+Eeveezen@users.noreply.github.com>
2023-09-27 15:14:44 +0300

    Add files via upload

--- src/Diena.js
+++ src/Diena.js
@@ -1,10 +1,45 @@
 import Stunda from "./Stunda";
 
 function Diena(props) {
+    const pirmdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const otradienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const tresdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const ceturdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const piektdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
     return (
     <>
         <p>sodien ir {props.nosaukums}</p>
-        <Stunda name="datortikli"/>
+        <ol>
+        <Stunda name={pirmdienasStundas[0]}/>
+        <Stunda name={pirmdienasStundas[1]}/>
+        <Stunda name={pirmdienasStundas[2]}/>
+        <Stunda name={pirmdienasStundas[3]}/>
+        </ol>
     </>
     );
 } 
--- src/stundas.js
+++ src/stundas.js
@@ -1,5 +1,35 @@
 function Stunda(props) {
     return <p>{props.name}</p>
+    const pirmdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const otradienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const tresdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const ceturdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
+    const piektdienasStundas = [
+        "datortikli",
+        "datortikli",
+        "programmesana",
+        "programmesana"
+    ];
 }
 ;
 
