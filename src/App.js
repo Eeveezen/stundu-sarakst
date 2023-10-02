cd4c6c050b9174cad070fa11ab747c3de3217216
Eeveezen <81543263+Eeveezen@users.noreply.github.com>
2023-10-02 13:54:21 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import Diena from "./Diena";
 
 function App() {
+  const pirmdienasStundas = [
+    "datortikli",
+    "datortikli",
+    "programmesana",
+    "programmesana"
+];
+const otradienasStundas = [
+    "datortikli",
+    "datortikli",
+    "programmesana",
+    "programmesana"
+];
+const tresdienasStundas = [
+    "datortikli",
+    "datortikli",
+    "programmesana",
+    "programmesana"
+];
+const ceturdienasStundas = [
+    "datortikli",
+    "datortikli",
+    "programmesana",
+    "programmesana"
+];
+const piektdienasStundas = [
+    "datortikli",
+    "datortikli",
+    "programmesana",
+    "programmesana"
+];
   return (
     <>
     <h1>
       pickmin
     </h1>
-      <Diena nosaukums="pirmdiena"/>
-      <Diena nosaukums="otradiena"/>
-      <Diena nosaukums="tresdiena"/>
-      <Diena nosaukums="ceturdiena"/>
-      <Diena nosaukums="piektdiena"/>
+      <Diena nosaukums="pirmdiena" Stundas={pirmdienasStundas}/>
+      <Diena nosaukums="otradiena" Stundas={otradienasStundas}/>
+      <Diena nosaukums="tresdiena" Stundas={tresdienasStundas}/>
+      <Diena nosaukums="ceturdiena" Stundas={ceturdienasStundas}/>
+      <Diena nosaukums="piektdiena" Stunda={piektdienasStundas}/>
     </>
   );
 }
--- src/Diena.js
+++ src/Diena.js
@@ -1,44 +1,16 @@
 import Stunda from "./Stunda";
 
 function Diena(props) {
-    const pirmdienasStundas = [
-        "datortikli",
-        "datortikli",
-        "programmesana",
-        "programmesana"
-    ];
-    const otradienasStundas = [
-        "datortikli",
-        "datortikli",
-        "programmesana",
-        "programmesana"
-    ];
-    const tresdienasStundas = [
-        "datortikli",
-        "datortikli",
-        "programmesana",
-        "programmesana"
-    ];
-    const ceturdienasStundas = [
-        "datortikli",
-        "datortikli",
-        "programmesana",
-        "programmesana"
-    ];
-    const piektdienasStundas = [
-        "datortikli",
-        "datortikli",
-        "programmesana",
-        "programmesana"
-    ];
+
+    const stunduSarakstsJSX = prop.stundas.map((stunda) => {
+        return <Stunda name={stunda}/>
+    })
+
     return (
     <>
         <p>sodien ir {props.nosaukums}</p>
         <ol>
-        <Stunda name={pirmdienasStundas[0]}/>
-        <Stunda name={pirmdienasStundas[1]}/>
-        <Stunda name={pirmdienasStundas[2]}/>
-        <Stunda name={pirmdienasStundas[3]}/>
+        {stunduSarakstsJSX}
         </ol>
     </>
     );
